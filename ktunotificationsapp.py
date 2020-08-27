1949f65cd5dd873013004d2f7b7be5ba4abc5ad6
iamivin <ivinignatius@gmail.com>
2020-08-27 17:31:37 +0530

    initial commit

---
+++ ktunotificationsapp.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+page = requests.get('https://ktu.edu.in/eu/core/announcements.htm')
+soup = BeautifulSoup(page.content, 'html.parser')
+contents = soup.find(class_='c-details')
+items= contents.find_all('tr')
+
+date = [item.find(class_='news-date').get_text() for item in items]
+maindata = []
+subdata = []
+i = 0
+while len(items)>i:
+    data = items[i].get_text().splitlines()
+    while ('' in data):
+        data.remove('')
+    maindata.append(data[3])
+    subdata.append(data[4:])
+    i = i + 1
+while ('' in maindata):
+    maindata.remove('')
+while ('' in subdata):
+    subdata.remove()
+
+ktunotifications = pd.DataFrame(
+    {
+        'Date': date,
+        'notificatin': maindata,
+        'details': subdata,
+    })
+
+
+#print(weatherdata)
+ktunotifications.to_csv('ktunotifications.csv')
